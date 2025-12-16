1eaf2464f868fea1d5e3d5e5147165542caf45ac
¨abdessamad¨ <abdobenzekri05.com>
2025-12-17 00:05:19 +0100

    fixing the time and hour inputs

--- src/pages/AjouterConsultation.jsx
+++ src/pages/AjouterConsultation.jsx
@@ -36,16 +36,19 @@ export default function AjouterConsultation() {
     <div className="max-w-5xl mx-auto mt-10 px-4">
     <form onSubmit={submit} className="bg-[#2f404f9d] mb-10 mt-10 rounded-lg p-6 space-y-4 shadow-2xl">
 
-      <input required className="border-2 rounded-lg outline-0 w-full p-2 border-white text-white" name="patient" placeholder="Patient" onChange={handleChange} />
+      <input required className="border-2 mt-5 rounded-lg outline-0 w-full p-2 border-white text-white" name="patient" placeholder="Patient" onChange={handleChange} />
+      <div className="mt-5">
+        <label htmlFor="" className="text-white">consultation date:</label>
       <input required className="border-2 rounded-lg outline-0 w-full p-2 border-white text-white" type="date" name="date" onChange={handleChange} />
-      <input required className="border-2 rounded-lg outline-0 w-full p-2 border-white text-white" name="diagnostic" placeholder="Diagnostic" onChange={handleChange} />
+      </div>
+      <input required className="border-2 mt-5 rounded-lg outline-0 w-full p-2 border-white text-white" name="diagnostic" placeholder="Diagnostic" onChange={handleChange} />
 
-      <select required className="border-2 rounded-lg outline-0 w-full p-2 border-white text-white" name="type" onChange={handleChange}>
+      <select required className="border-2 mt-5 rounded-lg outline-0 w-full p-2 border-white text-white" name="type" onChange={handleChange}>
         <option className="text-black">Consultation</option>
         <option className="text-black">Contrôle</option>
         <option className="text-black">Certificat</option>
       </select>
-      <button  className="group text-white relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium transition-all [box-shadow:0px_4px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none" type="submit">Créer</button>
+      <button  className="group mt-5 text-white relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium transition-all [box-shadow:0px_4px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none" type="submit">Créer</button>
     </form>
     </div>
     </>
--- src/pages/AjouterRDV.jsx
+++ src/pages/AjouterRDV.jsx
@@ -36,13 +36,19 @@ export default function AjouterRdv() {
 
     <form onSubmit={submit}  className="bg-[#2f404f9d] mb-10 mt-10 rounded-lg p-6 space-y-4 shadow-2xl">
        
-      <input required className="border-2 rounded-lg outline-0 w-full p-2 border-white text-white"  name="patient" placeholder="Patient" onChange={handleChange} id="patient" />
+      <input required className="border-2 mt-5 rounded-lg outline-0 w-full p-2 border-white text-white"  name="patient" placeholder="Patient" onChange={handleChange} id="patient" />
+      <div className="mt-5">
+        <label htmlFor="" className="text-white">Date de rendez-vous:</label>
       <input required className="border-2 rounded-lg outline-0 w-full p-2 border-white text-white"  name="date" type="date" onChange={handleChange} id="date" />
+      </div>
+      <div className="mt-5">
+        <label htmlFor="" className="text-white">Heure de rendez-vous:</label>
       <input required className="border-2 rounded-lg outline-0 w-full p-2 border-white text-white"  name="heure" type="time" onChange={handleChange} id="heure" />
+      </div>
       
       
 
-      <select required className="border-2 rounded-lg outline-0 w-full p-2 border-white text-white" name="motif" onChange={handleChange} id="motif">
+      <select required className="border-2 mt-5 rounded-lg outline-0 w-full p-2 border-white text-white" name="motif" onChange={handleChange} id="motif">
         <option className="text-black">Consultation</option>
         <option className="text-black">Contrôle</option>
         <option className="text-black">Certificat</option>
@@ -51,7 +57,7 @@ export default function AjouterRdv() {
       </select>
       
 
-      <button className="group text-white relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium transition-all [box-shadow:0px_4px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none" type="submit" id="sub">Créer</button>
+      <button className="group mt-5 text-white relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium transition-all [box-shadow:0px_4px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none" type="submit" id="sub">Créer</button>
     </form>
     </div>
     </>
