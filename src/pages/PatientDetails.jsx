d21d5002bdcd36b9f172538ec05c8ac49629967f
¨abdessamad¨ <abdobenzekri05.com>
2025-12-16 23:48:34 +0100

    small color changes and gapping in the patient details

--- src/pages/PatientDetails.jsx
+++ src/pages/PatientDetails.jsx
@@ -19,38 +19,38 @@ export default function PatientDetails() {
   return (
     <div className="max-w-xl mx-auto mt-10 p-6 rounded-2xl shadow-xl hover:shadow-2xl">
       
-      <div className="bg-gradient-to-r from-blue-400 to-blue-600 p-4 rounded-xl mb-6 text-white text-center shadow-md flex items-center justify-center gap-2">
+      <div className="bg-gradient-to-r from-[#3894A1] to-[#2F404F] p-4 rounded-xl mb-6 text-white text-center shadow-md flex items-center justify-center gap-2">
         <FaUserMd className="text-white text-2xl" />
         <h1 className="text-3xl font-bold">Dossier Médical</h1>
       </div>
 
       <div className="space-y-3 text-gray-800">
-        <p className="flex items-center gap-2 font-semibold">
-          <span className="font-semibold bg-blue-100 text-blue-800 px-2 py-1 rounded-full shadow-sm">
+        <p className="flex items-center gap-5 font-semibold">
+          <span className="font-semibold text-blue-800">
             Age
           </span> 
           {calculateAge(patient.dateNaissance)} ans
         </p>
-        <p className="flex items-center gap-2 font-semibold">
+        <p className="flex items-center gap-5 font-semibold">
           <FaPhone className="text-blue-500" /> {patient.telephone}
         </p>
-        <p className="flex items-center gap-2 font-semibold">
+        <p className="flex items-center gap-5 font-semibold">
           <FaMapMarkerAlt className="text-blue-500" /> {patient.adresse}
         </p>
-        <p className="flex items-center gap-2 font-semibold">
+        <p className="flex items-center gap-5 font-semibold">
           <FaEnvelope className="text-blue-500" /> {patient.email}
         </p>
-        <p className="flex items-center gap-2 font-semibold">
+        <p className="flex items-center gap-5 font-semibold">
           <FaTint className="text-red-500" /> Groupe sanguin: {patient.groupeSanguin}
         </p>
-        <p className="flex items-center gap-2 font-semibold">
+        <p className="flex items-center gap-5 font-semibold">
           <FaExclamationTriangle className="text-yellow-500" /> Allergies: {patient.allergies}
         </p>
       </div>
 
       <Link
         to={`/patients/ajouter?id=${patient.id}`}
-        className="mt-6 inline-block w-full text-center px-6 py-3 bg-gradient-to-r from-green-400 to-teal-500 text-white font-semibold rounded-xl shadow-lg hover:from-green-500 hover:to-teal-600 transition-all duration-300"
+        className="mt-6 inline-block w-full text-center px-6 py-3 bg-gradient-to-r from-[#C7DAD3] to-teal-500 text-white font-semibold rounded-xl shadow-lg hover:from-green-500 hover:to-teal-600 transition-all duration-300"
       >
         Modifier
       </Link>
